d9ea22b28857298247cf8dec8470e246af97f101
bastien-muraccioli <bastien.muraccioli.pro@gmail.com>
2026-06-10 15:16:53 +0900

    Update default config for new bota driver

--- src/RosImuSensor.cpp
+++ src/RosImuSensor.cpp
@@ -16,37 +16,34 @@ void RosImuSensor::init(mc_control::MCGlobalController & controller, const mc_rt
 
   if(!ctl.controller().datastore().has("ros_spin"))
   {
-     ctl.controller().datastore().make<bool>("ros_spin", false);
+    ctl.controller().datastore().make<bool>("ros_spin", false);
   }
   // load config
   referenceFrame = config("reference_frame", (std::string) "FT_sensor_imu");
   verbose = config("verbose", false);
-  ros_imu_sensor_ = config("ros_imu_sensor", false);
-  imu_sensor_topic_ = config("ros_topic_sensor", (std::string) "/bus0/ft_sensor0/ft_sensor_readings/imu");
-  freq_ = config("imu_freq", (double) 1000);
+  imu_sensor_topic_ = config("ros_topic_sensor", (std::string) "/bota_ft_sensor/imu");
+  freq_ = config("imu_freq", (double)1000);
   bodySensor_name_ = config("body_sensor_name", (std::string) "Accelerometer");
   // config loaded
-  maxTime_ = 1/freq_;
+  maxTime_ = 1 / freq_;
 
-  if(ros_imu_sensor_)
+  // Initializing ROS node
+  node = mc_rtc::ROSBridge::get_node_handle();
+  if(!ctl.controller().datastore().get<bool>("ros_spin"))
   {
-    // Initializing ROS node
-    node = mc_rtc::ROSBridge::get_node_handle();
-    if(!ctl.controller().datastore().get<bool>("ros_spin"))
-    {
-      spinThread_ = std::thread(std::bind(&RosImuSensor::rosSpinner, this));
-      ctl.controller().datastore().assign("ros_spin", true);
-    }
-    mc_rtc::log::info("[RosImuSensor][ROS] Subscribing to {}", imu_sensor_topic_);
-
-    imu_sub_.subscribe(node, imu_sensor_topic_);
-    imu_sub_.maxTime(maxTime_);
+    spinThread_ = std::thread(std::bind(&RosImuSensor::rosSpinner, this));
+    ctl.controller().datastore().assign("ros_spin", true);
   }
+  mc_rtc::log::info("[RosImuSensor][ROS] Subscribing to {}", imu_sensor_topic_);
+
+  imu_sub_.subscribe(node, imu_sensor_topic_);
+  imu_sub_.maxTime(maxTime_);
 
   // Check if the body sensor exists
   if(!controller.controller().robot().hasBodySensor(bodySensor_name_))
   {
-    mc_rtc::log::info("[RosImuSensor] Body sensor {} does not exist in the robot. Creating Body Sensor...", bodySensor_name_);
+    mc_rtc::log::info("[RosImuSensor] Body sensor {} does not exist in the robot. Creating Body Sensor...",
+                      bodySensor_name_);
     // Create the body sensor
     mc_rbdyn::BodySensor imu_sensor(bodySensor_name_, referenceFrame, sva::PTransformd::Identity());
     // Add the body sensor to the robot
@@ -60,19 +57,23 @@ void RosImuSensor::init(mc_control::MCGlobalController & controller, const mc_rt
   linear_acceleration_ = imu_sub_.data().value().linear();
   angular_velocity_ = imu_sub_.data().value().angular();
 
-  ctl.controller().gui()->addElement({"Plugins", "IMU"},
-                                    mc_rtc::gui::ArrayLabel(
-                                        "EndEffector",{"ax", "ay", "az", "ωx", "ωy", "ωz"},[this, &controller]()
-                                        {
-                                          auto linearAcceleration = controller.controller().robot().bodySensor(bodySensor_name_).linearAcceleration().transpose();
-                                          auto angularVelocity = controller.controller().robot().bodySensor(bodySensor_name_).angularVelocity().transpose();
-                                          return std::vector<double>{linearAcceleration.x(), linearAcceleration.y(), linearAcceleration.z(), angularVelocity.x(), angularVelocity.y(), angularVelocity.z()};
-                                        }));
+  ctl.controller().gui()->addElement(
+      {"Plugins", "RosIMUSensor"},
+      mc_rtc::gui::ArrayLabel(
+          "EndEffector", {"ax", "ay", "az", "ωx", "ωy", "ωz"},
+          [this, &controller]()
+          {
+            auto linearAcceleration =
+                controller.controller().robot().bodySensor(bodySensor_name_).linearAcceleration().transpose();
+            auto angularVelocity =
+                controller.controller().robot().bodySensor(bodySensor_name_).angularVelocity().transpose();
+            return std::vector<double>{linearAcceleration.x(), linearAcceleration.y(), linearAcceleration.z(),
+                                       angularVelocity.x(),    angularVelocity.y(),    angularVelocity.z()};
+          }));
 
   // Log
   ctl.controller().logger().addLogEntry("RosImuSensor_LinearAcceleration", [this]() { return linear_acceleration_; });
   ctl.controller().logger().addLogEntry("RosImuSensor_AngularVelocity", [this]() { return angular_velocity_; });
-  
 }
 
 void RosImuSensor::reset(mc_control::MCGlobalController & controller)
@@ -82,19 +83,12 @@ void RosImuSensor::reset(mc_control::MCGlobalController & controller)
 
 void RosImuSensor::before(mc_control::MCGlobalController & controller)
 {
-
-  //imuBodySensor = mc_rbdyn::BodySensor(bodySensor_name_, referenceFrame, sva::PTransformd::Identity());
   auto & ctl = static_cast<mc_control::MCGlobalController &>(controller);
   auto & robot = ctl.robot();
   linear_acceleration_ = imu_sub_.data().value().linear();
   angular_velocity_ = imu_sub_.data().value().angular();
   ctl.setSensorLinearAccelerations({{bodySensor_name_, linear_acceleration_}});
   ctl.setSensorAngularVelocities({{bodySensor_name_, angular_velocity_}});
-  
-  // mc_rtc::log::info("[RosImuSensor][ROS] Linear acceleration: {} | Angular velocity: {}",
-  //   robot.bodySensor(bodySensor_name_).linearAcceleration().transpose(),
-  //   robot.bodySensor(bodySensor_name_).angularVelocity().transpose()
-  // );
 }
 
 void RosImuSensor::after(mc_control::MCGlobalController & controller)
@@ -114,21 +108,21 @@ mc_control::GlobalPlugin::GlobalPluginConfiguration RosImuSensor::configuration(
 void RosImuSensor::rosSpinner(void)
 {
   mc_rtc::log::info("[RosImuSensor][ROS Spinner] thread created for imu sensor reading");
-  #ifdef MC_RTC_ROS_IS_ROS2
-    rclcpp::Rate r(freq_);
-    while(rclcpp::ok())
-    {
-      rclcpp::spin_some(node);
-      r.sleep();
-    }
-  #else
-    ros::Rate r(freq_);
-    while(ros::ok())
-    {
-      ros::spinOnce();
-      r.sleep();
-    }
-  #endif
+#ifdef MC_RTC_ROS_IS_ROS2
+  rclcpp::Rate r(freq_);
+  while(rclcpp::ok())
+  {
+    rclcpp::spin_some(node);
+    r.sleep();
+  }
+#else
+  ros::Rate r(freq_);
+  while(ros::ok())
+  {
+    ros::spinOnce();
+    r.sleep();
+  }
+#endif
   mc_rtc::log::info("[RosImuSensor][ROS Spinner] spinner destroyed");
 }
 
--- src/RosImuSensor.h
+++ src/RosImuSensor.h
@@ -7,11 +7,11 @@
 #include <mc_control/GlobalPlugin.h>
 #include <mc_rbdyn/BodySensor.h>
 #ifdef MC_RTC_ROS_IS_ROS2
-  #include "utils/ROS2Subscriber.h"
-  #pragma message("Using ROS2Subscriber.h")
+#  include "utils/ROS2Subscriber.h"
+#  pragma message("Using ROS2Subscriber.h")
 #else
-  #include "utils/ROSSubscriber.h"
-  #pragma message("Using ROSSubscriber.h")
+#  include "utils/ROSSubscriber.h"
+#  pragma message("Using ROSSubscriber.h")
 #endif
 
 namespace mc_plugin
@@ -32,27 +32,24 @@ struct RosImuSensor : public mc_control::GlobalPlugin
   ~RosImuSensor() override;
 
 private:
-  
   // IMU Sensor ROS
-  bool ros_imu_sensor_;       // Flag to enable/disable the IMU sensor
-  std::shared_ptr<rclcpp::Node> node; 
-  std::thread spinThread_;              // Thread to spin the ROS node
-  std::mutex mutex_;                    // Mutex to lock the IMU sensor data
+  std::shared_ptr<rclcpp::Node> node;
+  std::thread spinThread_; // Thread to spin the ROS node
+  std::mutex mutex_; // Mutex to lock the IMU sensor data
   ROSImuSubscriber imu_sub_; // ROS IMU subscriber
-  void rosSpinner(void);     // ROS spinner function
+  void rosSpinner(void); // ROS spinner function
 
   // yaml config
   bool verbose; // Verbose flag
   std::string referenceFrame; // Reference frame for the IMU sensor
-  double freq_;                 // Frequency of the IMU sensor
+  double freq_; // Frequency of the IMU sensor
   std::string imu_sensor_topic_; // IMU sensor topic
   std::string bodySensor_name_; // Name of the body sensor
   Eigen::Vector3d linear_acceleration_; // Linear acceleration
   Eigen::Vector3d angular_velocity_; // Angular velocity
 
-  double maxTime_;              // Maximum time for the IMU sensor
+  double maxTime_; // Maximum time for the IMU sensor
   // mc_rbdyn::BodySensor & imuBodySensor; // Body sensor for the IMU sensor
-  
 };
 
 } // namespace mc_plugin
